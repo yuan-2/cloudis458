360afd56d7e1e7ced86df9e1ba9bcf5844e7d931
yuan-2 <yycao.2019@smu.edu.sg>
2022-03-15 21:12:26 +0800

    Merge branch 'yuanyuan' of https://github.com/yuan-2/cloudis458 into yuanyuan

--- assets/db/tigershop.sql
+++ assets/db/tigershop.sql
@@ -6,6 +6,7 @@ DROP TABLE IF EXISTS `user`;
 CREATE TABLE IF NOT EXISTS `user` (
   `username` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
   PRIMARY KEY (`username`)
 ) ;
 
